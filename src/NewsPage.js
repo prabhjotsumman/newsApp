70ceb68d58a09e81fe3d2a4fd9a4cfbce54e1b89
prabhjotsumman <prabhjot.nith@gmail.com>
2022-04-28 04:50:15 -0400

    <NewsPage/> added

---
+++ src/NewsPage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { StyleSheet } from 'react-native';
+import { Carousel, Text } from 'react-native-ui-lib';
+
+import NewsCard from './components/NewsCard';
+import useNews from './useNews';
+
+const NewsPage = () => {
+  const { news } = useNews();
+
+  const handlePageChange = (newPageIndex, OldPageIndex) => {};
+  console.log(news);
+
+  return (
+    <Carousel horizontal={false} onChangePage={handlePageChange}>
+      {news.length ? (
+        news.map((item, index) => (
+          <NewsCard
+            key={index}
+            title={item?.title}
+            image={item?.imageUri}
+            content={item?.content}
+            author={item?.author}
+            postedAt={item?.postedAt}
+          />
+        ))
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </Carousel>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 24,
+    borderWidth: 2,
+    borderColor: 'white',
+    height: '100%',
+  },
+  index: {
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+});
+
+export default NewsPage;
