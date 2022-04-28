de521a6bbdbe2291788ab61b96b73c5eabcf494a
prabhjotsumman <prabhjot.nith@gmail.com>
2022-04-28 14:46:18 -0400

    useNeews updated

--- src/useNews.js
+++ src/useNews.js
@@ -1,45 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import inshorts from 'inshorts-api';
-
-const categories = [
-  'national',
-  'business',
-  'sports',
-  'world',
-  'politics',
-  'technology',
-  'startup',
-  'entertainment',
-  'miscellaneous',
-  'hatke',
-  'science',
-  'automobile',
-];
-
-var options = {
-  lang: 'en',
-  category: '',
-  numOfResults: 25,
-};
+import inshorts from './inshorts-api';
 
 const useNews = () => {
   const [news, setNews] = useState([]);
+  const [category, setCategory] = useState(0);
 
-  useEffect(() => {
+  const categories = [
+    'national',
+    'business',
+    'sports',
+    'world',
+    'politics',
+    'technology',
+    'startup',
+    'entertainment',
+    'miscellaneous',
+    'hatke',
+    'science',
+    'automobile',
+  ];
+
+  const fetchMoreNews = () => {
     try {
+      const options = {
+        lang: 'en',
+        category: categories[category],
+        numOfResults: 25,
+      };
+
       inshorts.get(options, function (result) {
-        setNews(result);
+        setNews([...news, ...result]);
+        if (category < categories.length) {
+          setCategory(category + 1);
+        }
       });
     } catch (error) {
       console.log(error);
     }
+  };
 
-    return () => {};
+  useEffect(() => {
+    fetchMoreNews();
   }, []);
 
   return {
     news,
+    fetchMoreNews,
   };
 };
 
