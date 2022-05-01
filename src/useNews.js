591412672b9940e501c254417f7ec882fd906616
prabhjotsumman <prabhjot.nith@gmail.com>
2022-05-01 16:40:36 -0400

    news sorted according to timestamp

--- src/useNews.js
+++ src/useNews.js
@@ -5,7 +5,8 @@ const useNews = () => {
     const news = await firestore().collection('news').get();
     const dataStore = news._docs;
     const newsDocs = dataStore.map(dataItem => dataItem._data);
-    console.log(' NEWS: ', newsDocs.length, newsDocs);
+    newsDocs.sort((a, b) => b.timestamp - a.timestamp);
+    console.log(' NEWS: ', newsDocs.length);
     return newsDocs;
   }
 
